e82140115745d56386a7688fcad4bac7d45ffbb1
Nelson <punkpunkada@gmail.com>
2023-05-13 17:31:34 -0300

    implementando teste

--- src/test/java/com/nelsonssoares/swtest/starwarstest/service/PlanetServiceTest.java
+++ src/test/java/com/nelsonssoares/swtest/starwarstest/service/PlanetServiceTest.java
@@ -5,16 +5,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
 import com.nelsonssoares.swtest.starwarstest.domain.Planet;
+import com.nelsonssoares.swtest.starwarstest.domain.QueryBuilder;
 import com.nelsonssoares.swtest.starwarstest.repository.PlanetRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+
+import javax.management.Query;
 
 import static com.nelsonssoares.swtest.starwarstest.common.PlanetConstants.PLANET;
 import static com.nelsonssoares.swtest.starwarstest.common.PlanetConstants.INVALID_PLANET;
@@ -87,4 +97,31 @@ public class PlanetServiceTest {
 
         assertThat(sut).isEmpty();
     }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets(){
+        List<Planet> planets = new ArrayList<>(){
+            {
+                add(PLANET);
+            }
+        };
+        Example<Planet> query = QueryBuilder.makeQuery(new Planet( PLANET.getTerrain(), PLANET.getClimate()));
+        when(planetRepository.findAll(query)).thenReturn(planets);
+        
+        List<Planet> sut = planetService.list(PLANET.getTerrain(), null, null);
+        assertThat(sut).isNotEmpty();
+        assertThat(sut).hasSize(1);
+        assertThat(sut.get(0)).isEqualTo(PLANET);
+    }
+/* 
+    @Test
+    public void listPlanets_ReturnsNoPlanets(){
+      when(planetRepository.findAll(any())).thenReturn(Collections.emptyList());
+
+      List<Planet> sut = planetService.list(PLANET.getTerrain(),PLANET.getClimate(), null);
+
+      assertThat(sut).isEmpty();
+
+    }
+    */
 }
